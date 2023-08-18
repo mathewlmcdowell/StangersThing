e71b8cd29e715c5a010971edc0128424d1e029bd
shawntaebatiste <shawntae.batiste@aol.com>
2023-08-18 18:57:59 -0500

    first commit

--- strangersThings/src/App.jsx
+++ strangersThings/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Posts from './components/Posts'
+import Profile from './components/Profile'
+import CreatePost from './components/CreatePost'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>Stranger's Things</h1>
+      <div id="container">
+        <div id="navbar">
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+          <Link to="/posts/new">Add A Post</Link>
+          <Link to="/posts">See All Post</Link>
+
+        </div>
+      </ div>
       <div>
+        <Routes>
+          <Route path='/' element={<Profile />} />
+          <Route path='/posts' element={<Posts />} />
+          <Route path='/posts/new' element={<CreatePost />} />
+        </Routes>
 
       </div>
     </>
