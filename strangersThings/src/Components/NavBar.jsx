ad667ecfb43cad7d6b307e797d944ed879f91192
Mathew <mathewlmcdowell@gmail.com>
2023-08-24 20:18:50 -0500

    moved login and register to upper corner

--- strangersThings/src/Components/NavBar.jsx
+++ strangersThings/src/Components/NavBar.jsx
@@ -4,18 +4,19 @@ import { Link } from "react-router-dom"
 export default function NavBar() {
     return (
         <div className='contain outline'>
-            <h1 className='title'>Stranger's Things</h1>
-                <div className='links'>
-                    
-                    <Link to='/' className='link-styles'>Home</Link>
-                    <Link to="/register" className='link-styles'>Register</Link>
-                    <Link to='/login' className='link-styles'>Login</Link>
-                    <Link to="/posts/new" className='link-styles'>Add A Post</Link>
-                    <Link to="/posts" className='link-styles'>See All Post</Link>
-                    
-                </div>
 
-            
+            <Link to="/register" className='link-styles2'>Register</Link>
+            <Link to='/login' className='link-styles2'>Login</Link>
+                
+                <h1 className='title'>Stranger's Things</h1>
+                    
+                    <div className='links'>
+                        
+                        <Link to='/' className='link-styles'>Home</Link>
+                        <Link to="/posts/new" className='link-styles'>Add A Post</Link>
+                        <Link to="/posts" className='link-styles'>See All Post</Link>
+                        
+                    </div>
         </div>
     )
 }
